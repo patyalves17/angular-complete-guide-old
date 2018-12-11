47dd4d5f3ecb47a1cdd8f66982b7ee5d63ab0c6f
patricia_aalmeida <patricia.almeida@fncit.com.br>
2018-12-11 09:11:08 -0200

    creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RepicesComponent } from './repices/repices.component';
+import { RecipeDetailComponent } from './repices/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './repices/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './repices/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepicesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/repices/repices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repices',
+  templateUrl: './repices.component.html',
+  styleUrls: ['./repices.component.css']
+})
+export class RepicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
