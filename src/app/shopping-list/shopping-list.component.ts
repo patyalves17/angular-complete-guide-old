8b8b9536f7be4cfa2b54591c9352acb11c45e069
Paty Alves <patriciaalves10@gmail.com>
2018-12-18 20:10:29 -0200

    shpping list edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+  ingredients:Ingredient[]=[
+    new Ingredient('tomatoes',5),
+    new Ingredient('potato',10),
+
+  ];
+  constructor() { 
+
+
+  }
 
   ngOnInit() {
   }
