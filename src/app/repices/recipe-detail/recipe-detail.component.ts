25f8ab1552f4baf11750082c61e569121f4f746f
Paty Alves <patriciaalves10@gmail.com>
2019-07-13 13:37:19 -0300

    remove comments, spaces, clear old code

--- src/app/repices/recipe-detail/recipe-detail.component.ts
+++ src/app/repices/recipe-detail/recipe-detail.component.ts
@@ -13,23 +13,17 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(private recipeService:RecipeService,
     private route:ActivatedRoute) { 
-      console.log('RecipeDetailComponent');
     }
 
   ngOnInit() {
-
     this.route.params.subscribe((params:Params)=>{
-      console.log(params);
       let id=+params['id'];
       this.recipe= this.recipeService.getRecipe(id);
-    });
-   
-    
+    });   
   }
 
-  // addToShoppingList(){
-  //   console.log("addToShoppingList");
-  //   this.recipeService.addRecipes(this.recipe.ingredients);
-  // }
+  addToShoppingList(){
+    this.recipeService.addRecipes(this.recipe.ingredients);
+  }
 
 }
