49e49fde0aba55b3dbe784a3535ff0e9bc85aa7b
Paty Alves <patriciaalves10@gmail.com>
2020-02-14 20:39:18 -0300

    sending login requests, login error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { AuthService, AuthResponseData } from "./auth.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-auth",
@@ -26,21 +27,32 @@ export class AuthComponent {
     const email = authForm.value.email;
     const password = authForm.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     if (this.isLoginMode) {
       //TODO implement login
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        resp => {
-          console.log(resp);
-          this.isLoading = false;
-          authForm.reset();
-        },
-        errorMessage => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
     }
+
+    authObs.subscribe(
+      resp => {
+        console.log(resp);
+        this.isLoading = false;
+        authForm.reset();
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+  }
+
+  handleError(errorMessage) {
+    console.log(errorMessage);
+    this.error = errorMessage;
+    this.isLoading = false;
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   email: string;
   expiresIn: string;
   idToken: string;
   kind: string;
   localId: string;
   refreshToken: string;
+  registered?: string;
 }
 
 @Injectable({ providedIn: "root" })
@@ -26,22 +27,51 @@ export class AuthService {
           returnSecureToken: true
         }
       )
-      .pipe(
-        catchError(errorResp => {
-          let errorMessage = "An error occurred";
+      .pipe(catchError(this.handleError));
+  }
 
-          if (!errorResp.error || !errorResp.error.error) {
-            return throwError(errorMessage);
-          }
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMFlnWfLIZGCmANVPmMhYEhwpr2OgkfYc",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
 
-          switch (errorResp.error.error.message) {
-            case "EMAIL_EXISTS":
-              errorMessage = "this emails exists already";
-              break;
-          }
+  private handleError(errorResp: HttpErrorResponse) {
+    let errorMessage = "An error occurred";
 
-          return throwError(errorMessage);
-        })
-      );
+    if (!errorResp.error || !errorResp.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorResp.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "this emails exists already.";
+        break;
+
+      case "INVALID_EMAIL":
+        errorMessage = "this emails is invalid.";
+        break;
+
+      case "INVALID_PASSWORD":
+        errorMessage = "this password is not correct.";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "this email does not exist.";
+        break;
+
+      case "USER_DISABLED":
+        errorMessage =
+          "This user account has been disabled by an administrator.";
+        break;
+    }
+
+    return throwError(errorMessage);
   }
 }
